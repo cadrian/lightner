16efe98abd93f8d938b12cf5a772a2477c50bb21
Cyril Adrian <cyril.adrian@gmail.com>
2023-09-09 21:59:03 +0200

    Revising debian packages

--- package-all.sh
+++ package-all.sh
@@ -2,11 +2,20 @@
 
 function prepareModule() {
     local module=$1
-    sudo dpkg -P lib$1-java
+
+    echo
+    echo Preparing $module
+
+    case $module in
+        lightner-main)
+            sudo dpkg -P $1-java
+            ;;
+        *)
+            sudo dpkg -P lib$1-java
+            ;;
+    esac
 
     (
-        echo
-        echo Preparing $module
         cp -al ../$module build/$module
     )
 }
@@ -14,42 +23,43 @@ function prepareModule() {
 function buildModule() {
     local module=$1
 
+    echo
+    echo Building $module
+
     (
-        echo
-        echo Building $module
-
         cd build/$module
+        mvn install -Dmaven.test.skip=true > mvn-install.log || exit 1
 
-        mvn install > mvn-install.log
-        case $module in
-            lightner-main)
-                # Special treatment for main: need to fill the right classpath in the main script
-                (
-                    mvn dependency:build-classpath -Dmdep.outputFile=src/scripts/cp.txt > getdep.log
-                    cd src/scripts
-                    sed "s!%CLASSPATH%!$(sed "s!$HOME/.m2/repository!/usr/share/maven-repo!g;s!/[^/]*/[^/]*jar!/debian/*!g" cp.txt)!" lightner.template > lightner
-                )
-                ;;
-            *)
-                :
-                ;;
-        esac
-        debuild -b -us -uc > build.log || {
-            echo "less -R pkg/$module/build.log"
-            exit 1
-        }
-        cd ..
-        sudo dpkg -i *.deb || exit 1
+        if [ -d debian ]; then
+            case $module in
+                lightner-main)
+                    # Special treatment for main: need to fill the right classpath in the main script
+                    (
+                        mvn dependency:build-classpath -DincludeScope=runtime -Dmdep.outputFile=src/scripts/cp.txt > getdep.log
+                        cd src/scripts
+                        sed "s!%CLASSPATH%!$(sed "s!$HOME/.m2/repository!/usr/share/maven-repo!g;s!/[^/]*/[^/]*jar!/debian/*!g" cp.txt)!" lightner.template > lightner
+                    )
+                    ;;
+                *)
+                    :
+                    ;;
+            esac
+            debuild -b -us -uc > build.log || {
+                echo "less -R pkg/build/$module/build.log"
+                exit 1
+            }
+            cd ..
+            sudo dpkg -i *.deb || exit 1
+            mv *.deb ../debs/
+        fi
     ) || exit 1
-
-    mv build/*.deb debs/
 }
 
 buildmsi() {
     # https://wiki.gnome.org/msitools/HowTo/CreateMSI
 
     echo
-    echo Build Windows MSI
+    echo Building Windows MSI
 
     mkdir -p win/src/lib win/src/jdk win/out
 
@@ -92,6 +102,13 @@ else
 
     rm -rf pkg $HOME/.m2/repository/net/cadrian/lightner
     mkdir -p pkg/build pkg/debs
+
+
+    echo
+    echo Testing
+
+    mvn test || exit 1
+
     cd pkg
 
     for module in main swing model root; do
